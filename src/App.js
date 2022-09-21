18d5686b05b8304927b95ec80b485e387aafc1cc
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-09-21 11:31:02 +0300

    Playground Created; - Ground added to playground; - Player added to playground;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Physics } from "@react-three/cannon";
+import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Ground } from "./components/Ground";
+import { Player } from "./components/Player";
 
 function App() {
   return (
-    <div>
-      <div>Outside Canvas</div>
-      <Canvas />
-    </div>
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 10]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Player />
+          <Ground />
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
